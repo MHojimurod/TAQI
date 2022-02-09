63bd7cdbb9f7c9682c573830a7a549f477013594
MHojimurod <cool.hojimurod2001@mail.ru>
2022-02-09 11:28:23 +0500

    fsdfds

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.urls import path
 
-from myapp.views import all_news,one_news
+from myapp.views import all_news,one_news,news_category
 
 
 
 
 urlpatterns = [
+    path("category",news_category),
     path("news/all",all_news),
     path("news/one",one_news)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -7,7 +7,7 @@ import json
 from myapp.models import News, NewsCategory
 
 
-class AllNews(generics.GenericAPIView):
+class NewsCat(generics.GenericAPIView):
     def get(self, request):
         data = NewsCategory.objects.all()
         a = {
@@ -17,11 +17,33 @@ class AllNews(generics.GenericAPIView):
         return JsonResponse(a, safe=False, json_dumps_params={'ensure_ascii': False})
 
 
+news_category = NewsCat.as_view()
+
+
+class AllNews(generics.GenericAPIView):
+
+    def get(self, request):
+        category = request.data
+        try:
+            if category['category_id']:
+                pass
+        except:
+            return JsonResponse({'ok': False, 'error': 'pk is required'}, json_dumps_params={'ensure_ascii': False})
+        news = News.objects.filter(news_category=category['category_id'])
+        if news:
+            a = {
+                "ok": True,
+                "news_category": NewsCategory.objects.get(pk=category["category_id"]).json,
+                "news": [i.json for i in news]
+            }
+            return JsonResponse(a, safe=False, json_dumps_params={'ensure_ascii': False})
+        return JsonResponse({"ok": False, "error": f"news is not found with category_id={category['category_id']}"}, safe=False, json_dumps_params={'ensure_ascii': False})
+
+
 all_news = AllNews.as_view()
 
 
 class OneNews(generics.GenericAPIView):
-
     def get(self, request):
         pk = request.data
         try:
@@ -29,15 +51,13 @@ class OneNews(generics.GenericAPIView):
                 pass
         except:
             return JsonResponse({'ok': False, 'error': 'pk is required'}, json_dumps_params={'ensure_ascii': False})
-        news = News.objects.filter(news_category=pk['pk'])
+        news = News.objects.filter(pk=pk['pk'])
         if news:
             a = {
                 "ok": True,
-                "news_category": NewsCategory.objects.get(pk=pk["pk"]).json,
-                "news": [i.json for i in news]
+                "news": news[0].json
             }
             return JsonResponse(a, safe=False, json_dumps_params={'ensure_ascii': False})
         return JsonResponse({"ok": False, "error": f"news is not found with pk={pk['pk']}"}, safe=False, json_dumps_params={'ensure_ascii': False})
-
 
 one_news = OneNews.as_view()
